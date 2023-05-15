8ba9d62d5d29a619d25924d11e41cebee3647d3f
valerii.korniienko <vakor.dev@gmail.com>
2023-05-15 14:23:46 +0300

    Optimized seq algorithm

--- lab2/src/SequentialAlgorithm.java
+++ lab2/src/SequentialAlgorithm.java
@@ -1,13 +1,14 @@
-public class MatrixMultiplication implements IMatrixMultiplicationAlgorithm {
+public class SequentialAlgorithm implements IMatrixMultiplicationAlgorithm {
     @Override
     public int[][] multiply(int[][] matrixA, int[][] matrixB) {
         int[][] result = new int[matrixA.length][matrixB[0].length];
         for (int i = 0; i < matrixA.length; i++) {
             for (int j = 0; j < matrixB[0].length; j++) {
-                result[i][j] = 0;
+                int value = 0;
                 for (int k = 0; k < matrixA[0].length; k++) {
-                    result[i][j] += matrixA[i][k] * matrixB[k][j];
+                    value += matrixA[i][k] * matrixB[k][j];
                 }
+                result[i][j] = value;
             }
         }
         return result;
