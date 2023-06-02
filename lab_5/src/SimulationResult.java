54c3d7704ff00db3ec6134f1db1aadab444a909c
valerii.korniienko <vakor.dev@gmail.com>
2023-06-02 11:25:42 +0300

    Lab 5 finished + docs

--- lab_5/src/SimulationResult.java
+++ lab_5/src/SimulationResult.java
@@ -27,29 +27,4 @@ public class SimulationResult {
         return averageQueueLength;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (SimulationResult) obj;
-        return this.rejectedCount == that.rejectedCount &&
-                this.servedCount == that.servedCount &&
-                Double.doubleToLongBits(this.chanceOfReject) == Double.doubleToLongBits(that.chanceOfReject) &&
-                Double.doubleToLongBits(this.averageQueueLength) == Double.doubleToLongBits(that.averageQueueLength);
-    }
-
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(rejected, served, chanceOfReject, averageQueueLength);
-//    }
-
-//    @Override
-//    public String toString() {
-//        return "SimulationResult[" +
-//                "rejected=" + rejected + ", " +
-//                "served=" + served + ", " +
-//                "chanceOfReject=" + chanceOfReject + ", " +
-//                "averageQueueLength=" + averageQueueLength + ']';
-//    }
-
 }
